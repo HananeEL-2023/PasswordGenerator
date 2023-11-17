ec0f21df7966c8dee5abf17bf1d6a3b5a5ca323a
Hanane <hanane.elkaaba1@gmail.com>
2023-11-17 14:32:48 +0100

    Generate password

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,115 @@
+import { useState } from "react";
+
 export default function App() {
+  const [choice, setChoice] = useState([]);
+  const [rangeInput, setRangeInput] = useState(5);
+  const [password, setPassword] = useState("");
+
+  //Function for randomize our password
+  const getRandomPassword = (basePassword) => {
+    let generatedPassword = "";
+
+    for (let i = 0; i < rangeInput; i++) {
+      const randomIndex = Math.floor(Math.random() * basePassword.length);
+
+      generatedPassword += basePassword.charAt(randomIndex);
+    }
+
+    setPassword(generatedPassword);
+  };
+
+  const GeneratePassword = (e) => {
+    e.preventDefault();
+    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowercase = "abcdefghijklmnopqrstuvwxyz";
+    const numbers = "0123456789";
+    const symbols = "!@#$%^&*()-=_+[]{}|;:,.<>?";
+
+    let basePassword = "";
+
+    choice.forEach((item) => {
+      if (item === "uppercase") basePassword += uppercase;
+      if (item === "lowercase") basePassword += lowercase;
+      if (item === "numbers") basePassword += numbers;
+      if (item === "symbols") basePassword += symbols;
+    });
+
+    getRandomPassword(basePassword);
+  };
+
+  const handleCheckBox = (e) => {
+    const { value, checked } = e.target;
+    if (checked) {
+      setChoice((pre) => [...pre, value]);
+    }
+  };
+
   return (
     <div>
       <div>
-        <p>The password is : </p>
+        <p>The password is : {password}</p>
         <button>Copier</button>
       </div>
-      <form action="">
+      <form onSubmit={GeneratePassword}>
         <div>
-          <input type="checkbox" name="uppercase" id="uppercase" />
+          <input
+            type="checkbox"
+            name="uppercase"
+            id="upperCase"
+            value="uppercase"
+            onChange={handleCheckBox}
+          />
           <label htmlFor="uppercase">Includes Uppercase Letters</label>
         </div>
         <div>
-          <input type="checkbox" name="lowerCase" id="lowerCase" />
+          <input
+            type="checkbox"
+            name="lowerCase"
+            id="lowerCase"
+            value="lowercase"
+            onChange={handleCheckBox}
+          />
           <label htmlFor="lowerCase">Includes LowerCase Letters</label>
         </div>
         <div>
-          <input type="checkbox" name="numbers" id="numbers" />
+          <input
+            type="checkbox"
+            name="numbers"
+            id="numbers"
+            value="numbers"
+            onChange={handleCheckBox}
+          />
           <label htmlFor="numbers">Includes Numbers</label>
         </div>
         <div>
-          <input type="checkbox" name="symbols" id="symbols" />
+          <input
+            type="checkbox"
+            name="symbols"
+            id="symbols"
+            value="symbols"
+            onChange={handleCheckBox}
+          />
           <label htmlFor="symbols">Includes Symbols</label>
         </div>
         <div>
-          <label htmlFor="range">Character length : </label>
-          <input type="range" name="" id="range" min="0" max="20" />
+          <input
+            type="range"
+            name="range"
+            id="range"
+            min="5"
+            max="20"
+            step="1"
+            value={rangeInput}
+            onChange={(e) => {
+              setRangeInput(e.target.value);
+            }}
+          />
+          <label htmlFor="range">Character length : {rangeInput}</label>
         </div>
-        <div>Strength : </div>
-        <button>Generate</button>
+        <div>
+          <p>Strength :</p>
+        </div>
+        <input type="submit" value="Generate" />
       </form>
     </div>
   );
